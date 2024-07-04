f0a575bfb84b9eaca9d2eab211337d0ca469778c
Perry Hargrave <bioe007@users.noreply.github.com>
2024-07-04 10:01:59 -0700

    Lexer working, end chapter 2.0

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -35,3 +35,86 @@ func TestNextToken(t *testing.T) {
 		}
 	}
 }
+
+func TestLetToken(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+    x + y;
+};
+let result = add(five, ten);
+!<>/*+-
+if else fn true false return
+
+10 == 10
+9 != 10
+`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FN, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.LT, "<"},
+		{token.GT, ">"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.PLUS, "+"},
+		{token.MINUS, "-"},
+		{token.IF, "if"},
+		{token.ELSE, "else"},
+		{token.FN, "fn"},
+		{token.TRUE, "true"},
+		{token.FALSE, "false"},
+		{token.RETURN, "return"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong: expected %q, got %q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - tokentype wrong: expected %q, got %q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
--- token/token.go
+++ token/token.go
@@ -21,12 +21,44 @@ const (
 	MINUS     = "-"
 	COMMA     = ","
 	SEMICOLON = ";"
-	LPAREN    = "("
-	RPAREN    = ")"
-	LBRACE    = "{"
-	RBRACE    = "}"
+	BANG      = "!"
+	SLASH     = "/"
+	ASTERISK  = "*"
+
+	LT = "<"
+	GT = ">"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	EQ     = "=="
+	NOT_EQ = "!="
 
 	// KEYWORDS
-	FUNCTION = "FUNCTION"
-	LET      = "LET"
+	FN     = "FUNCTION"
+	LET    = "LET"
+	TRUE   = "TRUE"
+	FALSE  = "FALSE"
+	IF     = "IF"
+	ELSE   = "ELSE"
+	RETURN = "RETURN"
 )
+
+var keywords = map[string]TokenType{
+	"fn":     FN,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
